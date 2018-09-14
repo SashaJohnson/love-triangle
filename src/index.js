62b0c1a2445305ff86f56a4e7435a7e8c39a084a
SashaJohnson <alexjohnson@yandex.ru>
2018-09-14 22:01:34 +0300

    first draft based on [2,3,1] example

--- src/index.js
+++ src/index.js
@@ -3,5 +3,32 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  
+  var count = 0;
+  var i = 0;
+  
+  for(i; i < preferences.length; i++) {
+      
+      if preferences.includes(preferences[i + 1]) {
+          
+          x = preferences.indexOf(i + 1);
+          
+          if preferences.includes(x + 1) {
+              
+              y = preferences.indexOf(x + 1);
+              
+              if (y + 1 ==  i + 1) {
+                  
+                  count++;
+                  
+              }
+              
+          }
+          
+      }
+    
+    console.log(count);
+  
+  
 };
+
