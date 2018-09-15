f4f0fb6542b03a6ddfcd7f7fedfc13e8e549ed8b
SashaJohnson <alexjohnson@yandex.ru>
2018-09-15 22:34:47 +0300

    fixed syntax error(missing function word)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = getLoveTrianglesCount(preferences = []) {
+module.exports = function getLoveTrianglesCount(preferences = []) {
 
     var count = 0;
     
